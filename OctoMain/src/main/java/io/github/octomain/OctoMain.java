92abcab29d07460a887622723ab359d186981e5d
Tagtart <tagtech123@gmail.com>
2015-04-09 19:12:52 +0100

    OctoMain

---
+++ OctoMain/src/main/java/io/github/octomain/OctoMain.java
@@ -0,0 +1,14 @@
+package io.github.octomain;
+
+public class OctoMain {
+
+	public OctoMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
